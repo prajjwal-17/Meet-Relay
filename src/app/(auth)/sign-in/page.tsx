16a00e03e0578111526160982b7d73adcc615fc4
prajjwal-17 <prajjwalchamp17@gmail.com>
2026-06-05 13:54:12 +0530

    Added Social Authentication

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,14 @@
 import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
-
-const Page = ()=>{
-    
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+const Page = async()=>{
+    const session = await auth.api.getSession({
+        headers : await headers(),
+      })
+      if(!!session){
+        redirect("/")
+      }
     return (
      <SignInView/>
      
